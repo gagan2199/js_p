205bb281dc0739a05892b0a6d0b4f680a1637f52
Gagan <gagank2199@gmail.com>
2023-08-19 16:27:39 +0530

    implicit conversions and parse int

---
+++ implicitconversions.js
@@ -0,0 +1,14 @@
+let num1="34"
+let num2="54"
+// num1=parseInt(num1);  if we use parse in it will purely converted into integer. + sign will also work
+// num2=parseInt(num2);
+let num3=num2-num1;
+let num4=num2+num1;
+let num5=num2/num1;
+let num6=num2*num1;
+
+document.write(num3+"<br/>")
+document.write(num5+"<br/>")
+document.write(num6+"<br/>")
+
+document.write(num4) //it will concatinate
