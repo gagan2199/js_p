a0030339d4bd93555dec1f816134fcdc701d3522
Gagan <gagank2199@gmail.com>
2023-08-20 18:44:43 +0530

    backticks

---
+++ backticks.js
@@ -0,0 +1,13 @@
+
+let age=22;
+fname="gagan kumar";
+
+let info=fname+ "age = "+ age+ "<br>";
+
+
+let inf= `the student name is ${fname} and age of that perticular student is ${age} ${"<br>"}`
+let infa= `the student name is ${fname} and age of that perticular student is ${age}`
+
+document.write(info);
+document.write(inf);
+document.write(infa);
