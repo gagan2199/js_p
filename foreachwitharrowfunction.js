44352cf3861f4ed366089e8e5c318526a817f0b1
Gagan <gagank2199@gmail.com>
2023-08-21 19:17:08 +0530

    foreach and arrow function

---
+++ foreachwitharrowfunction.js
@@ -0,0 +1,11 @@
+let emp=[
+    {nam:"ram",pos:"back end",rank:1},
+    {nam:"sham",pos:"front end",rank:2},
+    {nam:"govind",pos:"mobile application",rank:3}
+]
+
+emp.forEach((person)=>{
+    console.log((person.pos).toUpperCase());
+}
+
+);
