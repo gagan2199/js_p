8d01c5281acc5544aa6b3891d798f00372d02921
Gagan <gagank2199@gmail.com>
2023-08-24 22:03:02 +0530

    className and classList correction

--- classname&classlist.js
+++ classname&classlist.js
@@ -8,7 +8,7 @@ const third = document.getElementById('third');
 // const classValue = first.className;
 // console.log(classValue);
 
-second.className[0] = 'colors text';
+// second.className = 'colors text';
 
 // third.classList.add('colors');
 // third.classList.add('text');
