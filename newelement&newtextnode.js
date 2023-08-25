b09064fda687f30c7f8d5edfce439a61a77800ed
Gagan <gagank2199@gmail.com>
2023-08-25 17:00:07 +0530

    insertbefore

--- newelement&newtextnode.js
+++ newelement&newtextnode.js
@@ -1,5 +1,9 @@
+let ib1=document.getElementById('ib');
+
 let div1=document.createElement('div');
 let txt= document.createTextNode('this is our div1');
 
 div1.appendChild(txt);
-document.body.appendChild(div1)
+// document.body.appendChild(div1)
+
+document.body.insertBefore(div1,ib);
