62316a038f6986d53f9d72d569ba830a3cd28b81
Gagan <gagank2199@gmail.com>
2023-08-25 17:53:45 +0530

    prepend,remove,textcontent

--- newelement&newtextnode.js
+++ newelement&newtextnode.js
@@ -3,7 +3,29 @@ let ib1=document.getElementById('ib');
 let div1=document.createElement('div');
 let txt= document.createTextNode('this is our div1');
 
+let smallheading= document.createElement('h2');
+smallheading.innerText=`This is our small heading`
+
+document.body.prepend(smallheading);
+
 div1.appendChild(txt);
 // document.body.appendChild(div1)
 
 document.body.insertBefore(div1,ib);
+
+// div1.remove();
+
+// document.body.removeChild(div1);
+
+randomVar= 'random value';
+
+let elm= document.createElement('ul');
+
+
+elm.innerHTML=`<li class="abc">${randomVar}</li> <li class="abc">${randomVar}</li> <li class="abc">${randomVar}</li>`
+
+document.body.appendChild(elm);
+
+
+div1.innerHTML=`this is innerhtml`                  //same work
+div1.textContent=`this is textcontent`              //same work
