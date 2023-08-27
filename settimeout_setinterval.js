6c3c0a3e86663e1eb79a70fafa52936874ad9feb
Gagan <gagank2199@gmail.com>
2023-08-27 16:53:08 +0530

    settimeout and setinterval

---
+++ settimeout_setinterval.js
@@ -0,0 +1,21 @@
+function showit(nam,age)
+{
+    console.log(`name of student is ${nam} and age is ${age}`)
+}
+
+setTimeout(showit,2000,'ram',23)
+
+setTimeout((marks) => {
+    // marks= Math.ceil((Math.random));
+   marks= Math.floor(Math.random() * 101);
+
+    console.log(`marks = ${marks}`)
+}, 3000);
+
+let marksheet=setInterval((marks) => {
+    // marks= Math.ceil((Math.random));
+   marks= Math.floor(Math.random() * 101);
+
+}, 2000);
+
+console.log(`marks = ${marksheet}`)
