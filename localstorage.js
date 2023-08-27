9de5ac64679deaa8201e9afef9140b1d5cdbd637
Gagan <gagank2199@gmail.com>
2023-08-27 16:14:40 +0530

    JSON, localstorage multiple items

--- localstorage.js
+++ localstorage.js
@@ -11,3 +11,27 @@ let n= localStorage.getItem('weight');
 // localStorage.clear();
 
 console.log(n);
+
+let friends=['ram','sham','gita'];
+ 
+localStorage.setItem('friend',JSON.stringify(friends));
+
+let frnd=JSON.parse(localStorage.getItem(friends))
+
+console.log(friends[2]);
+
+let fruits;
+
+if(localStorage.getItem('fruits')){
+fruits= JSON.parse(localStorage.getItem('fruits'));
+}
+else{
+    fruits=[];
+}
+
+fruits.push('mango');
+fruits.push('orange');
+
+console.log(fruits);
+ 
+localStorage.setItem('fruits',JSON.stringify(fruits));
