9f8a5c1bb35a74d1867e17c02c9115a6c973d3b5
Gagan Kumar <114229570+gagan2199@users.noreply.github.com>
2023-10-25 19:22:52 +0530

    Update backticks.js

--- backticks.js
+++ backticks.js
@@ -5,7 +5,7 @@ fname="gagan kumar";
 let info=fname+ "age = "+ age+ "<br>";
 
 
-let inf= `the student name is ${fname} and age of that perticular student is ${age} ${"<br>"}`
+let inf= `the student name is ${fname} and age of that perticular student is equal to ${age} ${"<br>"}`
 let infa= `the student name is ${fname} and age of that perticular student is ${age}`
 
 document.write(info);
